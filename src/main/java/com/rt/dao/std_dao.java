ab2467efb53cc92b5572cfc8bab01679062df73c
nawaj <nzjmulani@gmail.com>
2023-08-25 23:06:34 -0700

    add custom query in jpql

--- src/main/java/com/rt/dao/std_dao.java
+++ src/main/java/com/rt/dao/std_dao.java
@@ -3,6 +3,8 @@ package com.rt.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.rt.entity.StudentEntity;
@@ -11,4 +13,6 @@ public interface std_dao extends JpaRepository<StudentEntity,Integer>{
 
 	List<StudentEntity> findByName(String name);
 	
+	@Query(value="select s from StudentEntity s where s.name=:n",nativeQuery=false)
+	List<StudentEntity> abc(@Param("n")String name);
 }
